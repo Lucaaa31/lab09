b0ceaee5c737a23151b2255e417d69ed8366fc70
Luca <luca.camillini310803@gmail.com>
2023-12-03 14:36:21 +0100

    Completed

--- 93-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUIWithFileChooser.java
+++ 93-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUIWithFileChooser.java
@@ -1,13 +1,20 @@
 package it.unibo.mvc;
 
+import java.awt.BorderLayout;
 import java.io.File;
 import java.io.IOException;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
-
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 /**
  * A very simple program using a graphical interface.
  * 
@@ -22,21 +29,21 @@ public final class SimpleGUIWithFileChooser {
     public SimpleGUIWithFileChooser(Controller controller) {
         JTextArea area = new JTextArea();
         JButton button = new JButton("Save");
+        JPanel mainPanel = new JPanel(new BorderLayout());
         area.setLineWrap(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setTitle("SimpleGUI");
+        frame.setTitle("SimpleGUIWithFileChoser");
         frame.setSize(400, 360);
-        frame.add(area);
-        frame.add(button, BorderLayout.SOUTH);
+        frame.add(mainPanel, BorderLayout.CENTER);
+        mainPanel.add(area);
+        mainPanel.add(button, BorderLayout.SOUTH);
+
+        controller.currentFile(file);
         
         button.addActionListener(new ActionListener() {
 
             @Override
             public void actionPerformed(ActionEvent arg0) {
-                Controller controller = new Controller();
-                
-
-                controller.currentFile(file);
                 
                 try {
                     controller.saveInFile(area.getText());
@@ -47,6 +54,37 @@ public final class SimpleGUIWithFileChooser {
             }
             
         });
+
+        JPanel upperPanel = new JPanel(new BorderLayout());
+        JTextField textField = new JTextField();
+        JButton browse = new JButton("Browse...");
+        upperPanel.add(textField);
+        upperPanel.add(browse, BorderLayout.EAST);
+        mainPanel.add(upperPanel, BorderLayout.NORTH);
+        textField.setText(file.toString());
+        textField.setEditable(false);
+
+        JFileChooser chooser = new JFileChooser();
+
+        browse.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                int result = chooser.showOpenDialog(chooser);
+                if (result == JFileChooser.APPROVE_OPTION) {
+                    controller.currentFile(chooser.getSelectedFile());
+                    textField.setText(controller.getFile().toString());
+                }else{
+                    if (result != JFileChooser.CANCEL_OPTION) {
+                        JOptionPane.showMessageDialog(frame, "un casino", "CAZZO", result);
+                    }
+                }
+                
+            }
+            
+        });
+        
+
     }
 
     public void display(){
@@ -54,7 +92,7 @@ public final class SimpleGUIWithFileChooser {
     }
 
     public static void main(String[] args) {
-        final SimpleGUI gui = new SimpleGUI(new Controller());
+        final SimpleGUIWithFileChooser gui = new SimpleGUIWithFileChooser(new Controller());
         gui.display();
     }
 
