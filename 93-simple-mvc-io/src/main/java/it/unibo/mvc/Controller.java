aac5d2456fb152f0ee3df7919493cf698f49003f
Luca <luca.camillini310803@gmail.com>
2023-12-03 13:29:36 +0100

    SimpleGUI - Completed

--- 93-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 93-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,32 @@
 package it.unibo.mvc;
 
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.OpenOption;
+import java.io.*;
 
 /**
  * Application controller. Performs the I/O.
  */
 public class Controller {
+    private File file;
 
+
+    public void currentFile(final File file){
+        this.file = file;
+    }
+
+    public File getFile(){
+        return this.file;
+    }
+
+    public String getPath(){
+        return file.getAbsolutePath();
+    }
+
+    public void saveInFile(final String str) throws IOException{
+        try(final PrintStream f = new PrintStream(file.getPath())){
+            f.print(str);
+        } 
+    }
 }
--- 93-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 93-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,6 +1,13 @@
 package it.unibo.mvc;
 
-import javax.swing.JFrame;
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+
+import javax.swing.*;
+import it.unibo.mvc.Controller;
 
 /**
  * A very simple program using a graphical interface.
@@ -9,5 +16,51 @@ import javax.swing.JFrame;
 public final class SimpleGUI {
 
     private final JFrame frame = new JFrame();
+    private File file = new File("/home/luca/Desktop/lab09/93-simple-mvc-io/Prova.txt");
+
+
+    public SimpleGUI(Controller controller) {
+        JTextArea area = new JTextArea();
+        JButton button = new JButton("Save");
+        area.setLineWrap(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("SimpleGUI");
+        frame.setSize(400, 360);
+        frame.add(area);
+        frame.add(button, BorderLayout.SOUTH);
+        
+        button.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                Controller controller = new Controller();
+                
+
+                controller.currentFile(file);
+                
+                try {
+                    controller.saveInFile(area.getText());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                
+            }
+            
+        });
+    }
+
+    public void display(){
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        final SimpleGUI gui = new SimpleGUI(new Controller());
+        gui.display();
+    }
+
+
+    private static SimpleGUI SimpleGUI() {
+        return null;
+    }
 
 }
